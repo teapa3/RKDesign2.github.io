a477ad56099434fabba938356095ffaf54a46021
teapa3 <82074550+teapa3@users.noreply.github.com>
2021-05-17 23:18:58 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,158 @@
+//テキストのカウントアップ+バーの設定
+var bar = new ProgressBar.Line(splash_text, {//id名を指定
+  easing: 'easeInOut',//アニメーション効果linear、easeIn、easeOut、easeInOutが指定可能
+  duration: 1000,//時間指定(1000＝1秒)
+  strokeWidth: 0.2,//進捗ゲージの太さ
+  color: '#555',//進捗ゲージのカラー
+  trailWidth: 0.2,//ゲージベースの線の太さ
+  trailColor: '#bbb',//ゲージベースの線のカラー
+  text: {//テキストの形状を直接指定       
+    style: {//天地中央に配置
+      position: 'absolute',
+      left: '50%',
+      top: '50%',
+      padding: '0',
+      margin: '-30px 0 0 0',//バーより上に配置
+      transform:'translate(-50%,-50%)',
+      'font-size':'1rem',
+      color: '#fff',
+    },
+    autoStyleContainer: false //自動付与のスタイルを切る
+  },
+  step: function(state, bar) {
+    bar.setText(Math.round(bar.value() * 100) + ' %'); //テキストの数値
+  }
+});
+
+//アニメーションスタート
+bar.animate(1.0, function () {//バーを描画する割合を指定します 1.0 なら100%まで描画します
+  $("#splash_text").fadeOut(10);//フェイドアウトでローディングテキストを削除
+  $(".loader_cover-up").addClass("coveranime");//カバーが上に上がるクラス追加
+  $(".loader_cover-down").addClass("coveranime");//カバーが下に下がるクラス追加
+  $("#splash").fadeOut();//#splashエリアをフェードアウト
+});
+
+//スクロールした際の動きを関数でまとめる
+function PageTopAnime() {
+    var scroll = $(window).scrollTop();
+    if (scroll >= 200){//上から200pxスクロールしたら
+      $('#page-top').removeClass('RightMove');//#page-topについているRightMoveというクラス名を除く
+      $('#page-top').addClass('LeftMove');//#page-topについているLeftMoveというクラス名を付与
+    }else{
+      if(
+        $('#page-top').hasClass('LeftMove')){//すでに#page-topにLeftMoveというクラス名がついていたら
+        $('#page-top').removeClass('LeftMove');//LeftMoveというクラス名を除き
+        $('#page-top').addClass('RightMove');//RightMoveというクラス名を#page-topに付与
+      }
+    }
+}
+
+// 画面をスクロールをしたら動かしたい場合の記述
+$(window).scroll(function () {
+  PageTopAnime();/* スクロールした際の動きの関数を呼ぶ*/
+});
+
+// ページが読み込まれたらすぐに動かしたい場合の記述
+$(window).on('load', function () {
+  PageTopAnime();/* スクロールした際の動きの関数を呼ぶ*/
+});
+
+
+// #page-topをクリックした際の設定
+$('#page-top').click(function () {
+    $('body,html').animate({
+        scrollTop: 0//ページトップまでスクロール
+    }, 500);//ページトップスクロールの速さ。数字が大きいほど遅くなる
+    return false;//リンク自体の無効化
+});
+//クリックするとフェードインを行う設定
+$(function() {
+ 
+  // ボタンをクリックしたら発動
+  $('onclick').click(function() {
+ 
+    // class'show'をdiv要素に追加
+    $('#fadeIn').addClass('show');
+ 
+  });
+});
+
+// お問い合わせフォーム
+
+(function(){
+  $('html').addClass('js');
+  
+  var contactForm = {
+    container: $('#contact'),
+    config: {
+      effect: 'slideToggle',
+      speed: 200
+    },
+    
+    init: function(config){
+      $.extend(this.config, config);
+      
+      $('#c-btn').on('click', this.show);
+    },
+
+    show: function(){
+      var cf = contactForm,
+          container = cf.container,
+          config = cf.config;
+                    
+
+      if(container.is(':hidden')){
+        cf.close.call(container);
+        container[config.effect]
+      }
+    },
+
+    close: function(){
+      var $this = $('#contact'); 
+      
+      if($this.find('span.close').length) return;
+
+      $('<span class=close>-</span>')
+        .prependTo(this)
+        .on('click', function(){
+        $this[contactForm.config.effect](contactForm.config.speed);
+      })
+    }
+  };
+
+contactForm.init({
+  effect: 'fadeToggle',
+  speed: 200
+});
+})();
+
+//profoleのfadein
+
+$(function(){
+	$(window).on('scroll',function (){
+		$('.animation').each(function(){
+			//ターゲットの位置を取得
+			var target = $(this).offset().top;
+			//スクロール量を取得
+			var scroll = $(window).scrollTop();
+			//ウィンドウの高さを取得
+			var height = $(window).height();
+			//ターゲットまでスクロールするとフェードインする
+			if (scroll > target - height){
+				//クラスを付与
+				$(this).addClass('active');
+			}
+		});
+	});
+});
+
+//ハンバーガーボタン
+$(".openbtn1").click(function () {//ボタンがクリックされたら
+  $(this).toggleClass('active');//ボタン自身に activeクラスを付与し
+    $("#g-nav").toggleClass('panelactive');//ナビゲーションにpanelactiveクラスを付与
+});
+
+$("#g-nav a").click(function () {//ナビゲーションのリンクがクリックされたら
+    $(".openbtn1").removeClass('active');//ボタンの activeクラスを除去し
+    $("#g-nav").removeClass('panelactive');//ナビゲーションのpanelactiveクラスも除去
+});
